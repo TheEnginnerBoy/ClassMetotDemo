b8a7cb091a77197839b126124f8eab5870414765
TheEnginnerBoy <78681893+TheEnginnerBoy@users.noreply.github.com>
2021-02-07 14:06:20 +0300

    ilk

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void musteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi:" + musteri.ad +" "+musteri.soyad);
+        }
+
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id+" Nolu musteri: "+musteri.ad + " " + musteri.soyad+" listeden silindi"); 
+        }
+
+        public void MusteriListeleme(Musteri[] musteriDizisi)
+        {
+            foreach (Musteri musteri in musteriDizisi)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("---------------------------");
+
+                Console.WriteLine("Müşteri Sırasi : " + musteri.Id);
+
+                Console.WriteLine("Müşteri İsmi : " + musteri.ad);
+
+                Console.WriteLine("Müşteri Soyadı : " + musteri.soyad);
+
+                Console.WriteLine("Müşteri Yaşı: " + musteri.yas);
+
+                Console.WriteLine("------------------------------");
+
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.ad = "ismail";
+            musteri1.soyad = "bozkut";
+            musteri1.yas = 35;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.ad = "mehmet";
+            musteri2.soyad = "alakuş";
+            musteri2.yas = 22;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.ad = "kenan";
+            musteri3.soyad = "kenanoglu";
+            musteri3.yas = 28;
+
+            Musteri[] musteriDizisi = new Musteri[] {musteri1,musteri2,musteri3};
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriListeleme(musteriDizisi);
+            musteriManager.musteriEkleme(musteri1);
+            musteriManager.MusteriSilme(musteri2);
+
+
+        }
+    }
+}
